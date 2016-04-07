66931f4c1f23f3b98637f96bd52bba7cef3ff049
Nishant Srivastava <nisrulz@gmail.com>
2016-04-07 12:21:04 +0530

    (*) updated misc

--- TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/misc/MiscUtils.java
+++ TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/misc/MiscUtils.java
@@ -2,6 +2,14 @@ package github.nisrulz.scripts.misc;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Environment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * The type Misc utils.
@@ -34,4 +42,55 @@ public class MiscUtils {
     }
 
 
+    /**
+     * Write String data to a csv file
+     *
+     * @param sFileName the s file name
+     * @param data      the data
+     * @throws IOException   the io exception
+     * @throws JSONException the json exception
+     */
+    public static void generateCsvFile(String sFileName, String data) throws IOException,
+            JSONException {
+        JSONObject objectToWrite = new JSONObject(data.toString());
+        File folder = new File(Environment.getExternalStorageDirectory() + "/Folder");
+
+        boolean var = false;
+        if (!folder.exists())
+            var = folder.mkdir();
+
+        System.out.println("" + var);
+
+
+        final String filename = folder.toString() + "/" + sFileName;
+
+
+        FileWriter writer = new FileWriter(filename, true);
+
+        try {
+
+            writer.append(objectToWrite.get("x").toString());
+            writer.append(',');
+            writer.append(objectToWrite.get("y").toString());
+            writer.append(',');
+            writer.append(objectToWrite.get("z").toString());
+            writer.append('\n');
+            writer.flush();
+            writer.close();
+
+
+        } catch (IOException e) {
+            writer.flush();
+            writer.close();
+            e.printStackTrace();
+        } catch (JSONException e) {
+            writer.flush();
+            writer.close();
+            e.printStackTrace();
+        } catch (Exception e) {
+            writer.flush();
+            writer.close();
+            e.printStackTrace();
+        }
+    }
 }
