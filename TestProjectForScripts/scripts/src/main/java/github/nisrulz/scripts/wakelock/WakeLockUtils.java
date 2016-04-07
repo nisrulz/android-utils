9afac4586f1c61e4b23f7ecad2570756697924aa
Nishant Srivastava <nisrulz@gmail.com>
2016-04-07 12:14:43 +0530

    (+) added battery

--- TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/wakelock/WakeLockUtils.java
+++ TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/wakelock/WakeLockUtils.java
@@ -4,17 +4,30 @@ import android.content.Context;
 import android.os.PowerManager;
 
 /**
+ * The type Wake lock utils.
+ *
  * @author Nishant Srivastava
  */
 public class WakeLockUtils {
+    /**
+     * The Wake lock.
+     */
     static PowerManager.WakeLock wakeLock;
 
+    /**
+     * Hold wake lock.
+     *
+     * @param context the context
+     */
     public static void holdWakeLock(Context context) {
         PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");
         wakeLock.acquire();
     }
 
+    /**
+     * Release wake lock.
+     */
     public static void releaseWakeLock() {
         if (wakeLock != null && wakeLock.isHeld()) {
             wakeLock.release();
