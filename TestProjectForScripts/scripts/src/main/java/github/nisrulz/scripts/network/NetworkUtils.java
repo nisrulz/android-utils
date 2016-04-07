80bc6fe0ae289237da812aa1004df608aa61c9fc
Nishant Srivastava <nisrulz@gmail.com>
2016-04-07 12:17:25 +0530

    (*) updated network

--- TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/network/NetworkUtils.java
+++ TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/network/NetworkUtils.java
@@ -1,10 +1,9 @@
 package github.nisrulz.scripts.network;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
 
 /**
  * The type Network utils.
@@ -12,26 +11,117 @@ import android.net.NetworkInfo;
  * @author Nishant Srivastava
  */
 public class NetworkUtils {
+
     /**
-     * Check if there is Internet Connection Available or not
+     * Get the network info
      *
      * @param context the context
-     * @return the boolean
+     * @return network info
      */
-    static boolean isNetworkAvailable(Context context) {
-        if (context.checkCallingOrSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
+    public static NetworkInfo getNetworkInfo(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
 
-            ConnectivityManager cm = (ConnectivityManager) context
-                    .getApplicationContext().getSystemService(
-                            Context.CONNECTIVITY_SERVICE);
-            NetworkInfo netInfo = cm.getActiveNetworkInfo();
-            if (netInfo != null && netInfo.isConnected()) {
-                return true;
-            } else {
-                return false;
+    /**
+     * Check if there is any connectivity
+     *
+     * @param context the context
+     * @return boolean
+     */
+    public static boolean isConnected(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null && info.isConnected());
+    }
+
+    /**
+     * Check if there is any connectivity to a Wifi network
+     *
+     * @param context the context
+     * @return boolean
+     */
+    public static boolean isConnectedWifi(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    /**
+     * Check if there is any connectivity to a mobile network
+     *
+     * @param context the context
+     * @return boolean
+     */
+    public static boolean isConnectedMobile(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+    /**
+     * Check if there is fast connectivity
+     *
+     * @param context the context
+     * @return boolean
+     */
+    public static boolean isConnectedFast(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null && info.isConnected() && isConnectionFast(info.getType(), info
+                .getSubtype()));
+    }
+
+    /**
+     * Check if the connection is fast
+     *
+     * @param type    the type
+     * @param subType the sub type
+     * @return boolean
+     */
+    public static boolean isConnectionFast(int type, int subType) {
+        if (type == ConnectivityManager.TYPE_WIFI) {
+            return true;
+        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+            switch (subType) {
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                    return false; // ~ 14-64 kbps
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                    return true; // ~ 400-1000 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                    return true; // ~ 600-1400 kbps
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                    return false; // ~ 100 kbps
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                    return true; // ~ 2-14 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                    return true; // ~ 700-1700 kbps
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                    return true; // ~ 1-23 Mbps
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                    return true; // ~ 400-7000 kbps
+            /*
+             * Above API level 7, make sure to set android:targetSdkVersion
+			 * to appropriate level to use these
+			 */
+                case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
+                    return true; // ~ 1-2 Mbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
+                    return true; // ~ 5 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
+                    return true; // ~ 10-20 Mbps
+                case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
+                    return false; // ~25 kbps
+                case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
+                    return true; // ~ 10+ Mbps
+                // Unknown
+                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                default:
+                    return false;
             }
+        } else {
+            return false;
         }
-        return false;
     }
 
 }
