74589dbb9f3d25d6035ae6fe9ec082b15c608a77
Nishant Srivastava <nisrulz@gmail.com>
2016-04-11 20:34:49 +0530

    (*) updated miscutils - ratemyapp function

--- TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/misc/MiscUtils.java
+++ TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/misc/MiscUtils.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -104,15 +105,27 @@ public class MiscUtils {
      * @param context the context
      */
     public static void rateMyApp(Context context) {
+
         Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
         Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
         try {
             context.startActivity(goToMarket);
         } catch (ActivityNotFoundException e) {
-            System.out.println("Couldn't launch the market");
+            Intent i = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + context
+                            .getPackageName()));
+            if (i.resolveActivity(context.getPackageManager()) != null)
+                context.startActivity(i);
+            else {
+                Toast.makeText(context, "Playstore Unavailable", Toast.LENGTH_SHORT).show();
+            }
         }
     }
-
 
     /**
      * Share my app.
