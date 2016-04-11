b26eeb59d2bf8c2def43d7229c9d95b2c5841638
Nishant Srivastava <nisrulz@gmail.com>
2016-04-11 14:14:10 +0530

    (*) updated MiscUtils

--- misc/MiscUtils.java
+++ misc/MiscUtils.java
@@ -1,7 +1,10 @@
 package github.nisrulz.scripts.misc;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Environment;
 
 import org.json.JSONException;
@@ -92,5 +95,61 @@ public class MiscUtils {
             writer.close();
             e.printStackTrace();
         }
+    }
+
+
+    /**
+     * Rate my app.
+     *
+     * @param context the context
+     */
+    public static void rateMyApp(Context context) {
+        Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        try {
+            context.startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            System.out.println("Couldn't launch the market");
+        }
+    }
+
+
+    /**
+     * Share my app.
+     *
+     * @param context the context
+     * @param subject the subject
+     * @param message the message
+     */
+    public static void shareMyApp(Context context, String subject, String message) {
+        try {
+            String appUrl = "https://play.google.com/store/apps/details?id=" + context.getPackageName();
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_SUBJECT, subject);
+            String leadingText = "\n" + message + "\n\n";
+            leadingText += appUrl + "\n\n";
+            i.putExtra(Intent.EXTRA_TEXT, leadingText);
+            context.startActivity(Intent.createChooser(i, "Share using"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Send mail.
+     *
+     * @param context  the context
+     * @param mailtoid the mailtoid
+     * @param subject  the subject
+     * @param body     the body
+     */
+    public static void sendMail(Context context, String mailtoid, String subject, String body) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", mailtoid, null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+        context.startActivity(Intent.createChooser(emailIntent, "Send email"));
+
     }
 }
