eef00d2f79756572e0d50f6dae32e4a828477cf7
Nishant Srivastava <nisrulz@gmail.com>
2016-04-20 11:05:20 +0530

    (+) added activity

---
+++ TestProjectForScripts/scripts/src/main/java/github/nisrulz/scripts/activity/ActivityUtils.java
@@ -0,0 +1,36 @@
+package github.nisrulz.scripts.activity;
+
+import android.app.Activity;
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * The type Activity utils.
+ */
+public class ActivityUtils {
+    /**
+     * Sets full screen.
+     *
+     * @param activity the activity
+     */
+    void setFullScreen(Activity activity) {
+        // Call before calling setContentView();
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager
+                .LayoutParams
+                .FLAG_FULLSCREEN);
+    }
+
+    /**
+     * Hide action bar.
+     *
+     * @param activity the activity
+     */
+    void hideActionBar(Activity activity) {
+        // Call before calling setContentView();
+        activity.getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            activity.getActionBar().hide();
+        }
+    }
+}
