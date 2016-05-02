8579347a8145160afd34a780f54b38370ee7ae2d
Nishant Srivastava <nisrulz@gmail.com>
2016-05-02 13:13:39 +0530

    (*) cleanup and code formatted (+) added enableStrictMode() function to MiscUtil

--- TestProjectForScripts/app/src/main/java/github/nisrulz/testprojectforscripts/MainActivity.java
+++ TestProjectForScripts/app/src/main/java/github/nisrulz/testprojectforscripts/MainActivity.java
@@ -1,13 +1,12 @@
 package github.nisrulz.testprojectforscripts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
