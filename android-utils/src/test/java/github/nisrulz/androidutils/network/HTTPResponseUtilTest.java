f87d68466b4dfb755f1ed3e89974c9db558a9180
Nishant Srivastava <nisrulz@gmail.com>
2017-01-31 23:29:51 -0800

    [*] code refactored

--- android-utils/src/test/java/github/nisrulz/androidutils/network/HTTPResponseUtilTest.java
+++ android-utils/src/test/java/github/nisrulz/androidutils/network/HTTPResponseUtilTest.java
@@ -3,13 +3,14 @@ package github.nisrulz.androidutils.network;
 import github.nisrulz.androidutils.BuildConfig;
 import github.nisrulz.androidutils.misc.MiscUtils;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
@@ -17,7 +18,7 @@ public class HTTPResponseUtilTest {
 
   HTTPResponseUtil httpResponseUtil;
   HTTPResponseInfo httpResponseInfo;
-  
+
 
   @Before
   public void setUp() throws Exception {
@@ -33,15 +34,15 @@ public class HTTPResponseUtilTest {
   public void getInfoWhenStatusCodeIs200() throws Exception {
     httpResponseInfo= httpResponseUtil.getInfo(200);
 
-    Assert.assertEquals(httpResponseInfo.getStatusCode(), 200);
-    Assert.assertEquals(httpResponseInfo.getStatusText(), "Ok");
-    Assert.assertEquals(httpResponseInfo.getStatusDescription(), "The request has succeeded. The meaning of a success varies depending on the HTTP method:\n"
+    assertEquals(httpResponseInfo.getStatusCode(), 200);
+    assertEquals(httpResponseInfo.getStatusText(), "Ok");
+    assertEquals(httpResponseInfo.getStatusDescription(), "The request has succeeded. The meaning of a success varies depending on the HTTP method:\n"
         + "GET: The resource has been fetched and is transmitted in the message body.\n"
         + "HEAD: The entity headers are in the message body.\n"
         + "POST: The resource describing the result of the action is transmitted in the message body.\n"
         + "TRACE: The message body contains the request message as received by the server");
 
-    Assert.assertEquals(httpResponseInfo.getType(), "Successful Response");
+    assertEquals(httpResponseInfo.getType(), "Successful Response");
 
   }
 
@@ -50,15 +51,15 @@ public class HTTPResponseUtilTest {
     int randomStatusCode = MiscUtils.getRandomNumberInRange(101,199);
     httpResponseInfo= httpResponseUtil.getInfo(randomStatusCode);
 
-    Assert.assertNotEquals(httpResponseInfo.getStatusCode(), 200);
-    Assert.assertNotEquals(httpResponseInfo.getStatusText(), "Ok");
-    Assert.assertNotEquals(httpResponseInfo.getStatusDescription(), "The request has succeeded. The meaning of a success varies depending on the HTTP method:\n"
+    assertNotEquals(httpResponseInfo.getStatusCode(), 200);
+    assertNotEquals(httpResponseInfo.getStatusText(), "Ok");
+    assertNotEquals(httpResponseInfo.getStatusDescription(), "The request has succeeded. The meaning of a success varies depending on the HTTP method:\n"
         + "GET: The resource has been fetched and is transmitted in the message body.\n"
         + "HEAD: The entity headers are in the message body.\n"
         + "POST: The resource describing the result of the action is transmitted in the message body.\n"
         + "TRACE: The message body contains the request message as received by the server");
 
-    Assert.assertNotEquals(httpResponseInfo.getType(), "Successful Response");
+    assertNotEquals(httpResponseInfo.getType(), "Successful Response");
 
   }
 }
