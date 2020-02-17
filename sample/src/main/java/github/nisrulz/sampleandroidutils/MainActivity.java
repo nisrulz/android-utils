3677d8898e6120dc3c906595cf4bbff80906b1ec
Nishant Srivastava <nisrulz@gmail.com>
2020-02-18 00:58:18 +0100

    fix main activity to use androidx

--- sample/src/main/java/github/nisrulz/sampleandroidutils/MainActivity.java
+++ sample/src/main/java/github/nisrulz/sampleandroidutils/MainActivity.java
@@ -17,7 +17,8 @@
 package github.nisrulz.sampleandroidutils;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
